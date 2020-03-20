dae0d40bd784493f045dacafecb16c2ccdf86b26
gytree <pedroguarimata@gmail.com>
2020-03-20 16:31:49 -0500

    clean build command.

--- src/Commands/BuildCommand.php
+++ src/Commands/BuildCommand.php
@@ -10,12 +10,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class BuildCommand extends Command
 {
-    protected static $defaultName = "components:build";
+    protected static $defaultName = "build";
 
     protected $work_path;
     protected $build_path;
     protected $components_path;
 
+    /** @var OutputInterface */
     protected $output;
 
     protected function configure()
@@ -30,19 +31,20 @@ class BuildCommand extends Command
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $this->output = $output;
-        $this->work_path  = $input->getArgument("path");
+        $this->work_path = $input->getArgument("path");
         if (!is_dir($this->work_path) and !mkdir($this->work_path)) {
             $output->writeln("Unable resolve the work path");
             return -1;
         }
-        $this->work_path = realpath($this->work_path);
+        $this->work_path = realpath($this->work_path) . DIRECTORY_SEPARATOR;
         if (!$this->prepareBuildPath()) {
             $output->writeln("Unable to prepare the build path");
             return -1;
         }
 
         $this->setupBuildSettings();
-        $this->components_path = $this->work_path . DIRECTORY_SEPARATOR . "components";
+        $this->components_path = $this->work_path . DIRECTORY_SEPARATOR .
+            "components" . DIRECTORY_SEPARATOR;
 
         if (!$this->buildComponentsIndex()) {
             $output->writeln("Unable to build the components index");
@@ -53,6 +55,8 @@ class BuildCommand extends Command
             $output->writeln("Unable to make the components build");
             return -1;
         }
+
+
         $output->writeln("build done");
         return 0;
     }
@@ -62,7 +66,7 @@ class BuildCommand extends Command
         $home = $_SERVER["HOME"];
         $hash = md5($this->work_path);
         $path = [$home, ".cache", "phpr", $hash];
-        $this->build_path = implode(DIRECTORY_SEPARATOR, $path);
+        $this->build_path = implode(DIRECTORY_SEPARATOR, $path) . DIRECTORY_SEPARATOR;
 
         if (is_dir($this->build_path)) {
             return true;
@@ -89,60 +93,56 @@ class BuildCommand extends Command
         if (!$this->prepareComponentsPath()) {
             return false;
         }
-        $files = array_diff(scandir($this->components_path), [".", "..", "index.js"]);
         $content = "";
         $exports = "";
+        $files = array_diff(scandir($this->components_path), [".", "..", "index.js"]);
         foreach ($files as $file_name) {
             $ext = substr($file_name, strrpos($file_name, "."));
             if (!in_array($ext, [".js", ".jsx"])) {
                 continue;
             }
             $component_name = substr($file_name, 0, strrpos($file_name, "."));
-            $component_path =  $this->components_path . DIRECTORY_SEPARATOR . $component_name;
+            $component_path = $this->components_path . $component_name;
             $content .= "import $component_name from '$component_path';";
             $exports .= (empty($exports) ? "" : ",") . $component_name;
         }
         $content .= "\nexport { $exports };";
-        $index_path = $this->build_path . DIRECTORY_SEPARATOR . "index.js";
+        $index_path = $this->build_path . "index.js";
         return file_put_contents($index_path, $content);
     }
 
     protected function prepareComponentsPath(): bool
     {
-        $this->components_path = $this->work_path . DIRECTORY_SEPARATOR . "components";
+        $this->components_path = $this->work_path . "components" . DIRECTORY_SEPARATOR;
         if (!is_dir($this->components_path)) {
             if (!mkdir($this->components_path)) {
                 return false;
             }
             $hello_component = Commands::ASSETS_PATH . DIRECTORY_SEPARATOR . "Hello.jsx";
-            copy($hello_component, $this->components_path . DIRECTORY_SEPARATOR . "Hello.jsx");
+            copy($hello_component, $this->components_path . "Hello.jsx");
         }
         return true;
     }
 
     protected function setupBuildSettings(): void
     {
-        $settings = ["output_path" => $this->work_path];
-        $settings_path = $this->work_path . DIRECTORY_SEPARATOR . "phpr.json";
-        if (is_file($settings_path)) {
-            $decoded = json_decode(file_get_contents($settings_path), true);
-            if (key_exists("output_path", $decoded)) {
+        $settings_path = $this->work_path . "phpr.json";
 
-                $path = $decoded["output_path"];
-                $path = realpath(implode(DIRECTORY_SEPARATOR, [$this->work_path, $path]));
-                if ($path === false) {
-                    $this->output->writeln(
-                        "Unable to resolve the setting output_path: "
-                            . $decoded["output_path"] . "\nmake sure that the path"
-                            . " exists and you have write permissions"
-                    );
-                } else if (is_dir($path)) {
-                    $settings["output_path"] = $path;
+        $settings = ["output" => $this->work_path];
+        if (is_file($settings_path)) {
+            $content = json_decode(file_get_contents($settings_path), true);
+            if ($content) {
+                $output = $settings["output"] ?? null;
+                if ($output and !is_dir($output)) {
+                    $output = realpath($this->work_path . $output) ?: $this->work_path;
+                    $content["output"] = $output;
                 }
+                $settings = array_merge($settings, $content);
             }
         }
-        $settings_file = $this->build_path . DIRECTORY_SEPARATOR . "settings.json";
-        file_put_contents($settings_file, json_encode($settings));
+
+        $build_settings = $this->build_path . "settings.json";
+        file_put_contents($build_settings, json_encode($settings));
     }
 
     protected function makeBuild(): bool
